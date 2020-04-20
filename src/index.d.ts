aa7e838bebcdea761336a6d9c23917d951d1466e
Ran Ribenzaft <ranrib@users.noreply.github.com>
2020-04-20 18:56:24 +0300

    fix(index.d.ts): add index.d.ts file (#29)
    * fix(index.d.ts): add index.d.ts file
    
    * Update index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,16 @@
+declare module 'epsagon' {
+  export function init(options: {
+    token: string
+    appName: string
+    metadataOnly?: boolean
+    useSSL?: boolean
+    traceCollectorURL?: string
+    isEpsagonDisabled?: boolean
+    urlPatternsToIgnore?: string[]
+    sendOnlyErrors?: boolean
+    sendTimeout?: number
+  }): void
+  export function label(key: string, value: string): void
+  export function setError(error: Error): void
+  export function ignoreEndpoints(endpoints: string[]): void
+}

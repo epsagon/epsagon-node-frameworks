443a3aed2c8e5db045e6b917a7c1ef4162a9598a
Itay Katz <itay@epsagon.com>
2020-09-27 11:46:37 +0300

    axios

--- examples/issues/tenna/index.js
+++ examples/issues/tenna/index.js
@@ -2,12 +2,13 @@
 // const epsagon = require("../../../src/index");
 const epsagon = require("epsagon-frameworks");
 const BunnyBus = require("@tenna-llc/bunnybus");
+var axios = require("axios");
 
-epsagon.init({
-  token: process.env.EPSAGON_TOKEN,
-  appName: "itay-bunnybus-test",
-  metadataOnly: false,
-});
+// epsagon.init({
+//   token: process.env.EPSAGON_TOKEN,
+//   appName: "itay-bunnybus-test",
+//   metadataOnly: false,
+// });
 
 const bunnyBus = new BunnyBus({
   hostname: "bonobo-01.rmq.cloudamqp.com",
@@ -32,6 +33,7 @@ async function subscribeHandlers() {
           // await sleep(200);
           epsagon.label("testKey", "testValue");
           console.log(message.comment);
+          await axios.get("http://dummy.restapiexample.com/api/v1/employees");
           await ack();
         },
       },
@@ -56,10 +58,10 @@ async function publishMessage() {
 
 subscribeHandlers();
 
-// setInterval(() => {
-//   publishMessage();
-//   console.log("message published");
-// }, 0);
+setInterval(() => {
+  publishMessage();
+  console.log("message published");
+}, 200);
 
 // const app = express();
 

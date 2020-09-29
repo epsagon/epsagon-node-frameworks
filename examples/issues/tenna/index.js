6f821e803eda97e40639b8994f74719d4aab12f7
Itay Katz <itay@epsagon.com>
2020-09-29 11:05:17 +0300

    tenna test

--- examples/issues/tenna/index.js
+++ examples/issues/tenna/index.js
@@ -1,22 +1,21 @@
 // const express = require("express");
 // const epsagon = require("../../../src/index");
-// const epsagon = require("epsagon-frameworks");
 const BunnyBus = require("@tenna-llc/bunnybus");
 var axios = require("axios");
+require("log-timestamp");
 
-// epsagon.init({
-//   token: process.env.EPSAGON_TOKEN,
-//   appName: "itay-bunnybus-test",
-//   metadataOnly: false,
-// });
+// const yargs = require("yargs");
+
+var myArgs = process.argv.slice(2);
 
 const bunnyBus = new BunnyBus({
   // hostname: "localhost",
-  hostname: "eager-deer-01.rmq.cloudamqp.com",
+  hostname: "adorable-hawk-01.rmq.cloudamqp.com",
   port: 5672,
-  password: "ibMZarLKO5ALU3jSRvIdpGwavIItpDKO",
-  username: "zqlqwfrd",
-  vhost: "zqlqwfrd",
+  password: "I_RVvw5641WUJFw9Kqjiwi80axL1cZLv",
+  username: "lcrliltv",
+  vhost: "lcrliltv",
+  prefetch: 50,
 });
 
 // function sleep(ms) {
@@ -25,33 +24,13 @@ const bunnyBus = new BunnyBus({
 //   });
 // }
 
-async function subscribeHandlers() {
-  try {
-    await bunnyBus.subscribe({
-      queue: "queue1",
-      handlers: {
-        "create-event": async ({ message, ack, epsagon }) => {
-          // await sleep(200);
-          epsagon.label("testKey", "testValue");
-          console.log(message.comment);
-          const res = await axios.get(
-            "http://dummy.restapiexample.com/api/v1/employees"
-          );
-          await ack();
-        },
-      },
-    });
-  } catch (err) {
-    console.log("failed to subscribe", err);
-  }
-}
-
 async function publishMessage() {
   try {
     bunnyBus.publish({
       message: {
         event: "create-event",
-        comment: "Test message",
+        options: { routeKey: "queue1" },
+        message: "Test message",
       },
     });
     console.log("message published");
@@ -60,28 +39,42 @@ async function publishMessage() {
   }
 }
 
-// subscribeHandlers();
+const operation = myArgs[0];
+const rate = myArgs[1];
+const period = myArgs[2];
 
-setInterval(async () => {
-  await Promise.all([
-    publishMessage(),
-    publishMessage(),
-    publishMessage(),
-    publishMessage(),
-    publishMessage(),
-    publishMessage(),
-    publishMessage(),
-    publishMessage(),
-    publishMessage(),
-    publishMessage(),
-  ]);
-}, 1);
+if (operation == "publish" && rate && period) {
+  const promises = Array.apply(null, Array(rate)).map(function () {
+    return publishMessage();
+  });
+  setInterval(async () => {
+    Promise.all(promises);
 
-// const app = express();
-
-// app.post("/", async (req, res) => {
-//   res.send("Message published!");
-//   await publishMessage();
-// });
-
-// app.listen(3000);
+    console.log("batch published");
+  }, period);
+} else if (operation === "subscribe") {
+  const epsagon = require("epsagon-frameworks");
+  epsagon.init({
+    token: process.env.EPSAGON_TOKEN,
+    appName: "itay-bunnybus-test",
+    metadataOnly: false,
+  });
+  try {
+    bunnyBus.subscribe({
+      queue: "queue1",
+      handlers: {
+        "create-event": async ({ message, ack, epsagon }) => {
+          // await sleep(200);
+          epsagon.label("testKey", "testValue");
+          console.log("message consumed");
+          // const res = await axios.get(
+          //   "http://dummy.restapiexample.com/api/v1/employees"
+          // );
+          await ack();
+        },
+      },
+    });
+  } catch (err) {
+    console.log("failed to subscribe", err);
+  }
+}

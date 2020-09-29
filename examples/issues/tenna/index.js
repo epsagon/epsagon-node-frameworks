fb3d6d7c3b6b83d140d35e943df0d18b141360c2
Itay Katz <itay@epsagon.com>
2020-09-29 12:13:30 +0300

    tenna test

--- examples/issues/tenna/index.js
+++ examples/issues/tenna/index.js
@@ -7,7 +7,7 @@ require("log-timestamp");
 // const yargs = require("yargs");
 
 var myArgs = process.argv.slice(2);
-
+const prefetch = myArgs[4];
 const bunnyBus = new BunnyBus({
   // hostname: "localhost",
   hostname: "fat-coral.rmq.cloudamqp.com",
@@ -15,7 +15,7 @@ const bunnyBus = new BunnyBus({
   password: "6GpPOPXCjVVLlgcnA_nmuYm2DNj3U57c",
   username: "ehqhhctv",
   vhost: "ehqhhctv",
-  prefetch: 50,
+  prefetch: prefetch,
 });
 
 // function sleep(ms) {
@@ -70,8 +70,10 @@ if (operation == "publish" && rate && period) {
     console.log("batch published");
   }, period);
 } else if (operation === "subscribe") {
-  if (epsagon) {
+  if (epsagon === "epsagon") {
     const epsagon = require("epsagon-frameworks");
+    console.log("init epsagon");
+
     epsagon.init({
       token: process.env.EPSAGON_TOKEN,
       appName: "itay-bunnybus-test",
@@ -87,8 +89,8 @@ if (operation == "publish" && rate && period) {
           // await sleep(200);
 
           console.log("message consumed");
-          axios.get("http://www.google.com");
-          axios.get("http://www.google.com");
+          // axios.get("http://www.google.com");
+          // axios.get("http://www.google.com");
 
           await ack();
         },

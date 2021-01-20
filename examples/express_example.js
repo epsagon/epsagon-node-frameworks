a9926cc19eb37faaf3475258242a53fd7acb9dd2
Itay Katz <itay@epsagon.com>
2021-01-20 10:26:00 +0000

    fix(mysql_temp): temp edit

--- examples/express_example.js
+++ examples/express_example.js
@@ -1,21 +1,66 @@
 const express = require('express');
-const epsagon = require('epsagon-frameworks');
+const epsagon = require('../src/index.js');
+var redis = require('redis');
 
 epsagon.init({
-    token: 'my-secret-token',
-    appName: 'my-app-name',
+    token: '57986555-4114-403f-b341-47e27385406a',
+    appName: 'madison-repro',
     metadataOnly: false,
 });
 
+var host = 'localhost'
+var redisPort = 6379
+var client = redis.createClient(redisPort, host, {});
+const mysql = require('mysql');
+ 
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'debian-sys-maint',
+  password: 'BXrw158wCBVXJZu0',
+  database: 'mysql'
+});
+ 
+//const Sequelize = require('sequelize');
+//const sequelize = new Sequelize('mysql', 'debian-sys-maint', 'BXrw158wCBVXJZu0', { host: 'localhost', dialect: 'mysql' });
+//var User = sequelize.define('user', { username: Sequelize.STRING, birthday: Sequelize.DATE });
+//sequelize.sync()
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/label_example', (req, res) => {
+app.get('/redis', (req, res) => {
     // Example label usage
-    req.epsagon.label('myFirstLabel', 'customValue1');
-    res.send('Hello World!')
+    setTimeout( () => {
+	    client.get('test', (err, result) => {
+		    req.epsagon.label('myFirstLabel', 'customValue1');
+		    res.send('Hello World!')
+	    });
+    }, 50);
 })
 
-
-app.listen(3000)
+app.get('/sql', (req, res) => {
+    // Example label usage
+    setTimeout( () => {
+	    client.get('test', (err, result) => {
+		    req.epsagon.label('myFirstLabel', 'customValue1');
+connection.query(
+  'SELECT * FROM `table` ',
+  function(err, results, fields) {
+	  console.log(err)
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+		    res.send('Hello World!')
+  }
+);
+	    });
+    }, 50);
+})
+app.get('/sqlize', (req, res) => {
+    client.get('test', (err, result)  => {
+        sequelize.query("SELECT * FROM `users`", { type: Sequelize.QueryTypes.SELECT }).then((response) => {
+          res.send('Hello World!')
+        })
+    });
+})
+app.listen(3000, () => {console.log("Server listening at http://localhost:3000")})
---
+++ examples/issues/tenna/index.js
@@ -0,0 +1,96 @@
+// const express = require("express");
+// const epsagon = require("../../../src/index");
+const BunnyBus = require("@tenna-llc/bunnybus");
+var axios = require("axios");
+require("log-timestamp");
+
+// const yargs = require("yargs");
+
+var myArgs = process.argv.slice(2);
+const bunnyBus = new BunnyBus({
+  // hostname: "localhost",
+  hostname: "creative-vulture.rmq.cloudamqp.com",
+  port: 5672,
+  password: "nNtqB5iZ34yBH_ypxrRDfFzPBDSt-42Q",
+  username: "gwzptqhx",
+  vhost: "gwzptqhx",
+  prefetch: 100,
+});
+const bunnyBus2 = new BunnyBus({
+  // hostname: "localhost",
+  hostname: "creative-vulture.rmq.cloudamqp.com",
+  port: 5672,
+  password: "nNtqB5iZ34yBH_ypxrRDfFzPBDSt-42Q",
+  username: "gwzptqhx",
+  vhost: "gwzptqhx",
+  prefetch: 50,
+});
+// function sleep(ms) {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, ms);
+//   });
+// }
+
+async function publishMessage(bunnyBus) {
+  try {
+    bunnyBus.publish({
+      message: {
+        event: "create-event",
+        options: { routeKey: "queue1" },
+        message: "Test message",
+      },
+    });
+    console.log("published message");
+  } catch (err) {
+    console.log("failed to publish", err);
+  }
+}
+
+const operation = myArgs[0];
+const rate = myArgs[1];
+const period = myArgs[2];
+const epsagon = myArgs[3];
+
+if (operation === "publish") {
+  // const promises = Array.apply(null, Array(rate)).map(function () {
+  //   return publishMessage();
+  // });
+  setInterval(async () => {
+    publishMessage(bunnyBus);
+    publishMessage(bunnyBus2);
+
+    console.log("batch published");
+  }, period);
+} else if (operation === "subscribe") {
+  if (epsagon === "epsagon") {
+    const epsagon = require("epsagon-frameworks");
+    // const epsagon = require("../../../src/index");
+
+    console.log("init epsagon");
+
+    epsagon.init({
+      token: process.env.EPSAGON_TOKEN,
+      appName: "itay-bunnybus-test",
+      metadataOnly: false,
+    });
+  }
+
+  try {
+    bunnyBus.subscribe({
+      queue: "queue1",
+      handlers: {
+        "create-event": async ({ message, ack, epsagon }) => {
+          // await sleep(200);
+
+          console.log("message consumed");
+          // axios.get("http://www.google.com");
+          // axios.get("http://www.google.com");
+
+          await ack();
+        },
+      },
+    });
+  } catch (err) {
+    console.log("failed to subscribe", err);
+  }
+}

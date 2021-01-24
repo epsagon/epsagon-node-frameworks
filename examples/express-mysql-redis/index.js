22d2fedd5fbf9784131669eaa0f1c3133cf1dd92
Haim Raitsev <haim@epsagon.com>
2021-01-24 18:42:23 +0200

    feat(performance_benchmark): dockerize express app

--- examples/express-mysql-redis/index.js
+++ examples/express-mysql-redis/index.js
@@ -1,34 +1,23 @@
 const express = require('express');
-const epsagon = require('../../src/index');
+const epsagon = require('epsagon');
 const redis = require('redis');
 const mysql = require('mysql');
-// const mysql2 = require('mysql2');
 const app = express();
-
-
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'secret',
-  database : 'mysql'
-});
-
-connection.connect();
 
 const pool = mysql.createPool(
 {
   connectionLimit: 60,
   waitForConnections: true,
   charset:"UTF8",
-  host     : 'localhost',
-  user     : 'root',
-  password : 'secret',
-  database : 'mysql'
+  host     : process.env.MYSQL_HOST || 'localhost',
+  user     : process.env.MYSQL_USER || 'root',
+  password : process.env.MYSQL_PASSWORD || 'secret',
+  database : process.env.MYSQL_DB || 'mysql'
 })
 
-var host = 'localhost' // '127.0.0.1';
-var redisPort = 6379;
-var client = redis.createClient(redisPort, host, {});
+const host = process.env.REDIS_HOST || 'localhost';
+const redisPort = process.env.REDIS_PORT || 6379;
+const client = redis.createClient(redisPort, host, {});
 
 epsagon.init({
     token: '5a407ec1-199b-439a-8d66-39aa9edf78f7',
@@ -38,51 +27,15 @@ epsagon.init({
     sendTimeout: 2000
   });
 
-app.get('/redis_sql_connection', async (req, res) => {
-    client.get('test', (err, result)  => {
-        if (err) throw err;
-        connection.query('SELECT 1 + 1 AS solution',async function (error, results, fields) {
-            if (error) throw error;
-            res.send('result from connection')
-        });
-    })
-})
-
 app.get('/redis_sql_pool', (req, res) => {
     client.get('test', (err, result)  => {
         if (err) throw err;
         pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
             if (error) throw error;
-            res.send('result from pool')
-        });        
-    })
-})
-
-app.get('/redis_sql_pool_connection', (req, res) => {
-    client.get('test', (err, result)  => {
-        if (err) throw err;
-        pool.getConnection(function(er, connection) {
-            if (er) throw er; // not connected!
-
-            // Use the connection
-            connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-                // When done with the connection, release it.
-                connection.release();
-
-                // Handle error after the release.
-                if (error) throw error;
-                res.send('result from pool coonection')
-            });
+            console.log("Express call log")
+            res.send('Express return from mysql pool')
         });
     })
-})
-
-app.get('/sql', (req, res) => {
-    res.send('sql demo')
-})
-
-app.get('/redis', (req, res) => {
-    res.send('redis demo')
 })
 
 app.listen(3000, () => {

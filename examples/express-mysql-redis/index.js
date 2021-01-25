a01305170766682e6021e1e75dd5161efeadbb93
Haim Raitsev <haim@epsagon.com>
2021-01-25 17:25:48 +0200

    fix(performance_benchmark): move out application from docker

--- examples/express-mysql-redis/index.js
+++ examples/express-mysql-redis/index.js
@@ -1,8 +1,111 @@
 const express = require('express');
 const epsagon = require('epsagon-frameworks');
+// const epsagon = require('../../src/index');
 const redis = require('redis');
 const mysql = require('mysql');
+// const path = require('path');
+// const Sentry = require('@sentry/node');
+// const bodyParser = require('body-parser')
+// const favicon = require('serve-favicon');
+// const compression = require('compression')
+// const cookieParser = require('cookie-parser')
+
 const app = express();
+
+// Sentry.init({ dsn: "https://examplePublicKey@o0.ingest.sentry.io/0" });
+
+// app.use(Sentry.Handlers.requestHandler());
+
+// app.use(function(req, res, next) { 
+//     Sentry.configureScope((scope) => { 
+//         scope.setTransaction(req.path);  
+//     });  
+//     next();
+// });
+
+// app.use(compression());
+// app.use(express.json({  
+//     type: function(req) { 
+//        return req.is('json') || req.is('text/plain'); 
+//      },
+//     limit: "50mb"
+// }));
+
+// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+// // app.use(require('connect-multiparty')(config.upload));
+// app.use(favicon(__dirname + '/public/favicon.ico', {  maxAge: 315360000})); //cache favicon
+
+// // if (!config.website.noCompileStyles) {
+// //       app.use(    
+// //         stylus.middleware({
+// //             src: path.resolve(__dirname),
+// //             dest: path.resolve(__dirname, 'public'),
+// //             debug: config.stylus.debug, 
+// //             compress: config.stylus.compress, 
+// //             force: config.stylus.force
+// //         })  
+// //     );
+// // }
+
+// // app.set('views', path.resolve(__dirname, 'views'));
+// // app.set('view engine', 'pug');
+// // app.set('view cache', config.pug.cache);
+// // app.locals.pretty = !config.pug.compress;
+// // app.use(shortUrlRedirect);
+// // app.use(requireHTTPS);
+
+// app.use(cookieParser())
+
+// // app.use('/scripts/thirdparty', express.static(path.resolve(__dirname, 'public/scripts/thirdparty'), {  maxAge: 86400000}));
+
+// // app.use('/ngpartials', function(req, res) { 
+// //     var view = req.originalUrl.substr(1).split('.html')[0];
+// //     res.render(view, req.query);
+// // });
+
+// app.use(express.static(path.resolve(__dirname, 'public')));
+// app.use(favicon(path.resolve(__dirname, 'public', 'favicon.ico')));
+
+// // if (config.logging.debug.express) {
+// //   // must come right after "static" so as not to log every static file request
+// //   app.use(morgan('dev'));
+// // }
+
+// // Set user info on apm
+// app.use(function(req, res, next) {
+// //   if (!objectUtils.getObjProperty(req, 'epsagon.label')) {
+// //     return next();
+// //   }
+
+// //   let userId = objectUtils.getObjProperty(req, 'session.user.id') ||
+// //     objectUtils.getObjProperty(req, 'dug');
+
+//   if (req.params.userId) {
+//     req.epsagon.label('userId',req.params.userId);
+//   }
+
+//   next();
+// })
+
+// // Set user info in sentry
+// app.use(function(req, res, next) {
+//   if (!req.session || !req.session.user) {
+//     return next();
+//   }
+
+//   Sentry.configureScope((scope) => {
+//     scope.setUser({
+//       email: "test",
+//       id: "tester-id"
+//     });
+//   });
+//   next();
+// });
+
+// // The error handler must be before any other error middleware and after all controllers
+// app.use(Sentry.Handlers.errorHandler());
+
+
 
 const pool = mysql.createPool(
 {
@@ -20,7 +123,7 @@ const redisPort = process.env.REDIS_PORT || 6379;
 const client = redis.createClient(redisPort, host, {});
 
 epsagon.init({
-    token: '5a407ec1-199b-439a-8d66-39aa9edf78f7',
+    token: 'e9ea2287-c552-4891-8639-b6e268b01c78',
     appName: 'haim-test',
     ignoredKeys:['password*'], // Ignore PII keys, properties
     metadataOnly: false,

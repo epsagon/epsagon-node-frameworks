b6976a3b46b85615c13afbf40189db3ab1f970ff
Tal Vintrob <tvaintrob@users.noreply.github.com>
2021-01-28 10:36:29 +0200

    feat(performance): monitor cpu/memory usage and disable epsagon if exceed limit (#102)
    * feat(performance): added resource monitor, disable epsagon if needed
    
    * feat(performance): fix linter errors

---
+++ src/resource_monitor/clear_tracers.js
@@ -0,0 +1,16 @@
+/** @fileoverview clear tracers every hour */
+
+const { utils } = require('epsagon');
+const schedule = require('node-schedule');
+const traceContext = require('../trace_context');
+
+const MAX_TRACERS = parseInt(process.env.EPSAGON_RESOURCE_MAX_TRACERS || '50', 10);
+const CLEAR_TRACERS_CRON_EXPR = process.env.EPSAGON_RESOURCE_CLEAR_TRACERS_CRON || '0 * * * *'; // every hour
+
+/** clear all tracers */
+function clearTracers() {
+    utils.debugLog('[resource-monitor] removing tracers from memory');
+    traceContext.privateClearTracers(MAX_TRACERS);
+}
+
+schedule.scheduleJob('clearTracers', CLEAR_TRACERS_CRON_EXPR, clearTracers);
---
+++ src/resource_monitor/monitor_resource_usage.js
@@ -0,0 +1,28 @@
+/** @fileoverview track cpu and memory usage and shutdown epsagon when necessary */
+
+const schedule = require('node-schedule');
+const epsagon = require('epsagon');
+const { getCPUUsage, getMemoryUsage } = require('./utils');
+
+const MAX_CPU_USAGE = parseInt(process.env.EPSAGON_RESOURCE_MAX_CPU || '90', 10) / 100;
+const MAX_MEM_USAGE = parseInt(process.env.EPSAGON_RESOURCE_MAX_MEM || '90', 10) / 100;
+const RESOURCE_MONITOR_CRON_EXPR = process.env.EPSAGON_RESOURCE_MONITOR_CRON || '* * * * *'; // every minute
+
+/** check cpu/mem usage */
+function monitorResources() {
+    epsagon.utils.debugLog('[resource-monitor] checking resource usage');
+    getCPUUsage().then((usedCPU) => {
+        const usedMemory = getMemoryUsage();
+
+        if (usedCPU > MAX_CPU_USAGE || usedMemory > MAX_MEM_USAGE) {
+            epsagon.utils.debugLog(
+                `[resource-monitor] cpu/mem exceeded allowed limit, disabling epsagon, cpu: ${usedCPU}, mem: %{usedMemory}`
+            );
+
+            // disable epsagon
+            epsagon.disable();
+        }
+    });
+}
+
+schedule.scheduleJob('monitorResources', RESOURCE_MONITOR_CRON_EXPR, monitorResources);
